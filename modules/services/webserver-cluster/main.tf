749e50c4e4745f17520e41791f907aa19cbbb7c2
cMoney667 <ococncol@gmail.com>
2025-06-20 15:51:35 -0400

    user setup and enable autoscaling

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -55,6 +55,29 @@ resource "aws_autoscaling_group" "example_autoscaling_group" {
   }
 }
 
+resource "aws_autoscaling_schedule" "scaling_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+
+  autoscaling_group_name = aws_autoscaling_group.example_autoscaling_group.name
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+
+  autoscaling_group_name = aws_autoscaling_group.example_autoscaling_group.name
+}
+
 resource "aws_lb" "example_lb" {
   name               = "${var.cluster_name}-asg"
   load_balancer_type = "application"
