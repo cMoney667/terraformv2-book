d6e85fa64a9aded88c62d39bc82d4c5b247edf2a
cMoney667 <ococncol@gmail.com>
2025-06-20 14:52:13 -0400

    wrapping up on modules

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,10 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 resource "aws_launch_template" "example_launch_template" {
   name_prefix            = "${var.cluster_name}-launch-template"
   image_id               = "ami-0fb653ca2d3203ac1"
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = base64encode(templatefile("user-data.sh", {
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
     server_port : var.server_port,
     db_address: data.terraform_remote_state.db.outputs.address,
     db_port: data.terraform_remote_state.db.outputs.port
@@ -105,20 +113,25 @@ resource "aws_lb_listener_rule" "lb_listener_rule" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-lb"
+}
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
 
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 data "aws_vpc" "default" {
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -20,3 +20,13 @@ module "webserver_cluster" {
   db_remote_state_bucket = "cmoney-terraform-up-and-running-state"
   db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
 }
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port = 12345
+  to_port = 12345
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
