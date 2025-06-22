7b81de33947e90f35af15be1a27fd05d85db0a2d
cMoney667 <ococncol@gmail.com>
2025-06-22 09:11:55 -0400

    for loops

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -8,3 +8,25 @@ module "users" {
   for_each = toset(var.user_names)
   user_name = each.value
 }
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type        = map(string)
+  default = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "mentor"
+  }
+}
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
+output "upper_names" {
+  value = [for name in var.user_names : upper(name)]
+}
+output "short_upper_names" {
+  value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
+output "for_directive" {
+  value = "%{ for i, name in var.user_names }(${i}) ${name}, %{ endfor }"
+}
